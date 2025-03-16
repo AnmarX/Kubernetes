568b977a7814b299cf7a8062939481eadcd6368f
Anmar <117080591+AnmarX@users.noreply.github.com>
2025-03-17 00:00:54 +0300

    alias

---
+++ kube_Example/Explanation/alias.sh
@@ -0,0 +1,21 @@
+# for win 11
+Set-Alias k "kubectl"
+
+# get the alias
+Get-Alias k
+
+# remove alisas
+Remove-Item Alias:k
+
+
+========================
+
+#for mac 
+alias k="kubectl"
+
+
+#check 
+alias k
+
+# all alias
+alias
